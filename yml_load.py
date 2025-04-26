123f6ac282247dd23eba0c97bb92bd7c8d5dae93
Facundo Galvagno <galvagnofacundo@gmail.com>
2025-04-25 22:36:18 -0300

    updated: yml_load now has proper get functions

--- yml_load.py
+++ yml_load.py
@@ -1,11 +1,38 @@
 import yaml 
 import glob
+from datetime import datetime as dt, timedelta as tdelta
+class lidarConfig:
+    def __init__(self, config_folder):
+        #config_dict.update({f.name[9:-4]: yaml.safe_load(f)})
+        
+        try:
+            with open(config_folder+'/config_global.yml', 'r') as f:
+                self.global_config = yaml.safe_load(f)
+            with open(config_folder+'/config_'+self.global_config['site']+'.yml', 'r') as f:
+                self.local_config = yaml.safe_load(f)
+        except FileNotFoundError as e:
+            print(f"Error: {e}")
+            print("Please check the configuration files.")
+        except yaml.YAMLError as e:
+            print(f"Error parsing YAML file: {e}")
+            print("Please check the configuration files.")
 
-def load_config():
-    config_dict = {}
-    cfg_files = glob.glob('./config/*.yml')
-    for file in cfg_files:
-        f = open(file,'r')
-        config_dict.update({f.name[9:-4]: yaml.safe_load(f)})
+        """
+        Initialize the lidar_config class with a dictionary of configuration settings.
+        
+        Args:
+            config_dict (dict): Dictionary containing configuration settings.
+        """
 
-    return config_dict
+    def get_src(self):
+        return self.local_config['src']
+    
+    def get_dateinterval(self):
+        if(self.global_config['plot_last_days']):
+            datearray = [dt.now()-tdelta(days=self.global_config['days_to_plot']), dt.now()]
+        else:
+            datearray = [dt.strptime(self.global_config['starting_date'], "%Y-%m-%d"), dt.strptime(self.global_config['end_date'], "%Y-%m-%d")]
+        return datearray
+
+    def get_channel(self):
+        return self.local_config['channel']
