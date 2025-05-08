2a109188d9468414d84b4d66ce8381852cc272b9
Facundo Galvagno <galvagno.facundo@outlook.com>
2025-05-08 17:45:26 -0300

    full implementation of plot

--- plot.py
+++ plot.py
@@ -4,16 +4,24 @@ import numpy as np
 
 custom_jet = ['rgb(255,255,255)','rgb(0,0,131)', 'rgb(0,60,170)', 'rgb(5,255,255)', 'rgb(255,255,0)', 'rgb(250,0,0)', 'rgb(128,0,0)']
     
-def plot_signal(volt_data, height_data, time_data, channel_name, use_log=False, limits = {'z_max': 20, 'z_min': -10, 'h_max': 15,}):
+def plot_signal(signal_set, time_data, channel_info, use_log=False, limits = {'z_max': 20, 'z_min': -10, 'h_max': 15,}):
+    if use_log is True:
+        z = np.log10(np.transpose(signal_set.values))
+        zmax = 3
+        zmin = -1
+    else:
+        z = np.transpose(signal_set.values)
+        zmax = limits['z_max']
+        zmin = limits['z_min']
     # Create the heatmap
     fig = go.Figure(data=go.Heatmap(
-        z=np.transpose(volt_data),  # Transpose to align bins (y) and time (x)
-        y=height_data[:-2048],  
+        z=z,  # Transpose to align bins (y) and time (x)
+        y=signal_set['height'].values,  
         x=time_data,
         #x=np.datetime_as_string(data_slice.coords['time'].values, unit='m'),  # Time on the x-axis
         colorbar=dict(title="Bin Value"),  # Add a color bar with a title
-        zmax = limits['z_max'],
-        zmin = limits['z_min'],
+        zmax = zmax,
+        zmin = zmin,
         colorscale=custom_jet,
         connectgaps = False
     ))  
@@ -25,11 +33,12 @@ def plot_signal(volt_data, height_data, time_data, channel_name, use_log=False, 
     # Añadir suavizado configurable 
     # Update layout
     fig.update_layout(
-        title=f"Señal Lidar {channel_name}",
+        title="Señal Lidar: " + str(channel_info['detected_wavelength']) + " nm",
         xaxis_title='Tiempo',
         yaxis_title='Altura (Km)',
         xaxis=dict(type="category", tickmode="linear", dtick = 72 ),  # Adjust time format if needed
         yaxis=dict(autorange=False, range=[0,limits['h_max']]),
+        plot_bgcolor='white'
         
     )
     
