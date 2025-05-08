5e11bd45b95127ca692cc8cb2a1c4f7cf294967a
Facundo Galvagno <galvagno.facundo@outlook.com>
2025-05-08 17:46:06 -0300

    updated main file to use xarray props

--- quicklook.py
+++ quicklook.py
@@ -1,9 +1,6 @@
-import os
 import numpy as np
 import xarray as xr
 import time
-import read_licel as rl
-import plot
 from datetime import datetime as dt, timedelta as tdelta
 from yml_load import lidarConfig
 import lidardsp
@@ -15,53 +12,38 @@ warnings.filterwarnings('ignore')
 
 lc = lidarConfig(config_folder="./config")
 
-directory = lc.src
-days = []
-date_interval = lc.get_dateinterval()
 
-try:
-    for f in os.scandir(directory):
-        if f.is_dir() and dt.strptime(f.name, "%Y%m%d") >= date_interval[0] and dt.strptime(f.name, "%Y%m%d") <= date_interval[1]:
-            print(f.name)
-            days.append(rl.dtfs(directory + "/" + f.name, optimize_reading=True, file_prefix=lc.location_prefix)[3]) #[1:2] metadata [3] data
-except Exception as e:
-    print(f"Error reading directory: {e}")
-    
-data = xr.concat([set for set in days if len(set)], dim='time')
-data = data.sortby('time')
-possible_channels = data.coords['channel'].values
-data = data.resample(time=tdelta(minutes=15), skipna=True).mean()
-channel_name = lc.channel  # First channel
-time_index = 0  
+signal, first_sample_channel_info = lidardsp.read_folder(directory=lc.src, date_interval=lc.get_dateinterval(), file_prefix=lc.location_prefix)
+
 
 #slice the data for the selected channel
-data_slice = data.sel(channel=channel_name)  # Slice by channel
+channel_name = lc.channel
+signal = signal.sel(channel=channel_name)  # Slice by channel
+signal = lidardsp.time_avg(signal, time=tdelta(minutes=lc.time_avg))
+
+channel_info = first_sample_channel_info.loc[channel_name]
+
 
 ##---------------APPLY L0-------------------##
-height = lidardsp.binnum2height(spatial_resol=lc.licel_parameters['spatial_resolution'], use_Km=True, bins=data_slice.coords['bins'].values)
-voltage = lidardsp.binval2volt(data_slice.values, lc.licel_parameters)
-voltage = lidardsp.bias_correction(voltage, bias_window = lc.bias_window)
-voltage = lidardsp.volts_height_correction(voltage, height, spatial_resol=lc.licel_parameters['spatial_resolution'])
+signal = signal.assign_coords(height = ('bins', lidardsp.binnum2height(signal.coords['bins'].values, channel_info, use_Km=True)))
+signal.values = lidardsp.binval2volt(signal, channel_info)
+signal.values = lidardsp.bias_correction(signal,  bias_window = lc.bias_window)
+signal.values = lidardsp.volts_height_correction(signal, channel_info)
 #voltage = spatial_moving_average(voltage, )
-
-timestamp = [np.datetime64(dt).astype('datetime64[s]').astype(object).strftime('%Y-%m-%d %H:%M') for dt in data_slice.coords['time'].values]
 ##---------------END OF L0-------------------##
-#voltage_bc = voltage - voltage[:,-1]
 
-plot_signal(volt_data=voltage, height_data=height, time_data=timestamp, channel_name=channel_name, limits=lc.plot_limits)
+signal = lidardsp.spatial_avg(signal, n=lc.spatial_avg_h)
+
+timestamp = [np.datetime64(dt).astype('datetime64[s]').astype(object).strftime('%Y-%m-%d %H:%M') for dt in signal.coords['time'].values]
+
+# use height as main dim coord
+signal = signal.swap_dims({"bins": "height"})
+# slice for max height
+signal = signal.sel(height=slice(0, lc.plot_limits['h_max']))
 
 
+plot_signal(signal_set = signal, time_data=timestamp, channel_info=channel_info, limits=lc.plot_limits, use_log=lc.use_log)
 print(time.time()-start)
-
-
-use_log = False
-
-#if use_log is True:
-#    z_axis = np.log10(np.transpose(voltage_avg)[:-2048])
-#else:
-#    z_axis = np.transpose(voltage_avg)[:-2048]
-
-
 
 
 
