45de997e521506fa9dc6d7f3600fa7bdf0d42412
Facundo Galvagno <galvagno.facundo@outlook.com>
2025-05-08 03:28:38 -0300

    updated: signal basic processing operations

--- lidardsp.py
+++ lidardsp.py
@@ -2,33 +2,35 @@ import pandas as pd
 import numpy as np
 
 
-def apply_L0(channel: str):
-    return
-
 def spatial_avg(channel: str):
+
     return
 
-def time_avg(channel: str):
+def time_avg(volt):
     return
 
-def log_y(channel: str):
+def offset_correction(volts, offset):
+    
     return
 
-def spatial_moving_average(volts, n=3):
-    volts = np.zeros((4082))
+def spatial_moving_average(volts, n=10):
+    volts = np.zeros(len(volts))
     for i in range(len(volts)):
         volts_avg = np.vstack([volts_avg, moving_average(volts[i], n)])
     return volts_avg
 
-def height_correction(volts, height, spatial_resol = 7.5)
-    return np.power(volts,2)/spatial_resol**2
-    return
+def volts_height_correction(volts, height, spatial_resol = 7.5):
+    return np.power(height,height)*volts/spatial_resol**2
 
-def binval2volt():
-    return
+def binval2volt(binval, lp={'volt_resolution': 50, 'bit_resolution': 16}):
+    return binval * (lp['volt_resolution']/(2**lp['bit_resolution']-1))
 
-def binnum2height():
-    return
+def binnum2height(spatial_resol = 7.5, use_Km = True, bins = []):
+    if use_Km:
+        return np.array(bins*spatial_resol/1e3)
+    else:
+        return np.array(bins*spatial_resol)
+
 
 def moving_average(a, n=3):
     ret = np.cumsum(a, dtype=float)
